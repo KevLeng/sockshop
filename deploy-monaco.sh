b2c4c472c4d17f51878ea3e5283626ab57423d75
Kevin Leng <kevin.leng@dynatrace.com>
2021-03-04 17:06:34 +1100

    updated echo output

--- deploy-monaco.sh
+++ deploy-monaco.sh
@@ -5,7 +5,7 @@
 monaco_version="v1.4.0"
 
 
-echo "Download Monaco"
+echo "Download Monaco & make executable"
 curl -L https://github.com/dynatrace-oss/dynatrace-monitoring-as-code/releases/download/{$monaco_version}/monaco-linux-amd64 --output monaco_cli
-echo "Make executable"
+
 chmod +x monaco_cli
