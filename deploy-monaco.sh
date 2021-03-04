3cf01dc591f234239ec9aa5c79df25afaca6aa6f
Kevin Leng <kevin.leng@dynatrace.com>
2021-03-04 20:28:15 +1100

    added in error, removed, added to push

--- deploy-monaco.sh
+++ deploy-monaco.sh
@@ -1,20 +1,7 @@
-
 #!/bin/bash
 # This script will install Monitoring as Code
 
 monaco_version="v1.4.0"
-DT_TENANT=${DT_TENANT:-none}
-DT_API_TOKEN=${DT_API_TOKEN:-none}
-
-
-if [[ "$DT_TENANT" == "none" ]]; then
-    echo "You have to set DT_TENANT to your Tenant URL, e.g: abc12345.dynatrace.live.com or yourdynatracemanaged.com/e/abcde-123123-asdfa-1231231"
-    exit 1
-fi
-if [[ "$DT_API_TOKEN" == "none" ]]; then
-    echo "You have to set DT_API_TOKEN to a Token that has read/write configuration, access metrics, log content and capture request data priviliges"
-    exit 1
-fi
 
 
 echo "Download Monaco & make executable"
