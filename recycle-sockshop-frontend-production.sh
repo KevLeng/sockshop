d618940423867af429bb211b34c828024aae6139
Kevin Leng <kevin.leng@dynatrace.com>
2021-06-15 14:01:09 +1000

    removed unused files

--- recycle-sockshop-frontend-production.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl scale --replicas=0 --current-replicas=1 deployments -l tier=frontend -n production
-sleep 10s
-kubectl get deployments -n production -l tier=frontend
-kubectl scale --replicas=1 --current-replicas=0 deployments -l tier=frontend -n production
-sleep 10s
-kubectl get deployments -n production -l tier=frontend
