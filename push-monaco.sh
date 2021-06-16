53a4978446c675bc187d972a4fa22c41a8048251
Kevin Leng <kevin.leng@dynatrace.com>
2021-06-16 11:05:24 +1000

    addedd DT_DASHBOARD_OWNER env variable

--- push-monaco.sh
+++ push-monaco.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 # This script will push the  Monitoring as Code config
 
+# Dynatrace Variables
 export DT_TENANT=${DT_TENANT:-none}
 export DT_API_TOKEN=${DT_API_TOKEN:-none}
+export DT_DEFAULT_GEOS=${DT_DEFAULT_GEOS:-true}
+export DT_DASHBOARD_OWNER=${DT_DASHBOARD_OWNER:-"kevin.leng@dynatrace.com"}
+
+# AZURE Variables
+export SKIP_AZURE=${SKIP_AZURE:-true}
 export AZURE_CLIENT_ID=${AZURE_CLIENT_ID:-none}
 export AZURE_TENANT_ID=${AZURE_TENANT_ID:-none}
 export AZURE_KEY=${AZURE_KEY:-none}
 
-export SKIP_AZURE=${SKIP_AZURE:-true}
-
-
+# Prometheus Variables
 export SKIP_PROMETHEUS=${SKIP_PROMETHEUS:-false}
 
-export DEFAULT_GEOS=${DEFAULT_GEOS:-true}
+
 
 if [[ "$DT_TENANT" == "none" ]]; then
     echo "You have to set DT_TENANT to your Tenant URL, e.g: abc12345.dynatrace.live.com or yourdynatracemanaged.com/e/abcde-123123-asdfa-1231231"
@@ -41,7 +45,7 @@ export synth_geo_location_id_1=${synth_geo_location_id_1:-"GEOLOCATION-8888B6EC3
 export synth_geo_location_id_2=${synth_geo_location_id_2:-"GEOLOCATION-C196364332B5D8E2"}
 
 if [[ "$DT_TENANT" == *"sprint"* ]]; then
-    if [[ "$DEFAULT_GEOS" == "true" ]]; then
+    if [[ "$DT_DEFAULT_GEOS" == "true" ]]; then
         echo "Sprint environment, using sprint geo location ids"
         export synth_geo_location_id_1="GEOLOCATION-0DF9A0E1095A5A62" #Portland
         export synth_geo_location_id_2="GEOLOCATION-E01B833216FC3598" #Hong Kong
@@ -49,6 +53,7 @@ if [[ "$DT_TENANT" == *"sprint"* ]]; then
 fi
 
 echo "Gathering environment details..."
+echo ""
 export dev_frontend_ip=$(kubectl describe svc front-end -n dev | grep "LoadBalancer Ingress:" | sed 's/LoadBalancer Ingress:[ \t]*//')
 export dev_carts_ip=$(kubectl describe svc carts -n dev | grep "LoadBalancer Ingress:" | sed 's/LoadBalancer Ingress:[ \t]*//')
 
@@ -79,11 +84,14 @@ if [[ $production_carts_ip == "" ]]; then
   export production_carts_ip=$(kubectl get ing carts -n production -o jsonpath='{.spec.rules[0].host}')
 fi
 
+echo "**************************************************"
+echo "Using the following:"
 echo "dev front-end: $dev_frontend_ip"
 echo "dev carts: $dev_carts_ip"
 echo "production front-end: $production_frontend_ip"
 echo "production carts: $production_carts_ip"
-
+echo "**************************************************"
+echo ""
 echo "Calling Monaco with sockshop config..."
 ./monaco_cli -e=monaco/sockshop-environment.yaml -p=sockshop monaco
 
