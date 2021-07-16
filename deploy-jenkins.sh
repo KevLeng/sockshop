e2ce30d146ed5d22776bca9bfff5caf0519f5ba5
Kevin Leng <kevin.leng@dynatrace.com>
2021-07-16 16:44:24 +1000

    jenkins deployment

---
+++ deploy-jenkins.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# This script will deploy jenkins for Monitoring as Code in a pipleine
+# assumes helm 3 is installed already
+
+PUBLIC_IP=$(curl -s ifconfig.me)
+PUBLIC_IP_AS_DOM=$(echo $PUBLIC_IP | sed 's~\.~-~g')
+NIP_DOMAIN="${PUBLIC_IP_AS_DOM}.nip.io"
+
+export DOMAIN=${DOMAIN:-$NIP_DOMAIN}
+export INGRESSCLASS=${INGRESSCLASS:-"istio"}
+
+echo "Create namespace"
+kubectl create ns jenkins
+echo "Deply Jenkins using helm"
+helm install jenkins jenkins/jenkins --set controller.jenkinsUrl=http://jenkins.$DOMAIN -f jenkins/values.yaml
+echo "Waiting for jenkins to start"
+kubectl wait po -n jenkins -l app.kubernetes.io/name=jenkins --for=condition=ready --timeout=120s
+
+echo "Deploy ingress"
+cat > ./sockshop-ingress.yaml <<- EOM
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  annotations:
+    kubernetes.io/ingress.class: $INGRESSCLASS
+  name: jenkins
+  namespace: jenkins
+spec:
+  rules:
+  - host: jenkins.$DOMAIN
+    http:
+      paths:
+      - path: /
+        pathType: Prefix
+        backend:
+          service:
+            name: jenkins
+            port:
+              number: 8080
+EOM
+
+kubectl apply -f sockshop-ingress.yaml
+rm sockshop-ingress.yaml
+
+
+echo "Jenkins Password is"
+
+kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/chart-admin-password && echo
+
